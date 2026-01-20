a879d908ab5d211c4cdb6b520d697b9350eacc24
Darssan Eswaramoorthi <darssan@email.sc.edu>
2026-01-20 01:10:39 -0500

    Update base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   // IMPORTANT for GitHub Pages under /tedxcongareevista/
-  base: process.env.VERCEL ? '/' : '/tedxcongareevista/',
+  base: '/',
 
   plugins: [react()],
   resolve: {
